4a14772d9d66e992788c64fe131495c9172cc12a
SKOINFO_MACBOOK <skoinfo_macbook@seukoinpomaegbug1ui-MacBook-Pro.local>
2015-11-27 14:21:02 +0900

    Initial Commit

---
+++ DaeguSchoolNavi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DaeguSchoolNavi
+//
+//  Created by SKOINFO_MACBOOK on 2015. 11. 27..
+//  Copyright (c) 2015년 SKOINFO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DaeguSchoolNavi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DaeguSchoolNavi
+//
+//  Created by SKOINFO_MACBOOK on 2015. 11. 27..
+//  Copyright (c) 2015년 SKOINFO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
